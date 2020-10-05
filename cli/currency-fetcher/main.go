622dd631a94aa1498d10e6660a73217a41a704b3
Dusan Malusev <dusan.998@outlook.com>
2020-10-05 08:22:40 +0200

    Starting the CLI
    Signed-off-by: Dusan Malusev <dusan.998@outlook.com>

--- cli/currency-fetcher/main.go
+++ cli/currency-fetcher/main.go
@@ -1,6 +1,97 @@
 package main
 
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"github.com/BrosSquad/currency-fetcher"
+	"github.com/BrosSquad/currency-fetcher/currency"
+	"github.com/BrosSquad/currency-fetcher/services"
+	"github.com/BrosSquad/currency-fetcher/storage"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/spf13/viper"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"strconv"
+)
 
 func main() {
+	var mongoDbClient *mongo.Client
+	var sqlDb *sql.DB
+	var err error
+	storages := make([]currency_fetcher.Storage, 0, 2)
+	ctx := context.Background()
 
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error while reading in the config file: %v", err)
+	}
+
+	for _, st := range viper.GetStringSlice("storage") {
+		switch st {
+		case "mysql":
+			config := viper.GetStringMapString("databases.mysql")
+			sqlDb, err = sql.Open("mysql", fmt.Sprintf("%s:%s@%s:%s/%s", config["user"], config["password"], config["host"], config["port"], config["db"]))
+			if err != nil {
+				log.Fatalf("Error while connecting to mysql: %v", err)
+			}
+
+			storages = append(storages, storage.NewMySQLStorage(ctx, sqlDb, config["table"], nil))
+		case "mongodb":
+			config := viper.GetStringMapString("databases.mongodb")
+			mongoDbClient, err = mongo.NewClient(options.Client().ApplyURI(config["uri"]))
+
+			if err != nil {
+				log.Fatalf("Error in mongo configuration: %v", err)
+			}
+
+			if err := mongoDbClient.Connect(ctx); err != nil {
+				log.Fatalf("Error while connecting to mongodb: %v", err)
+			}
+
+			db := mongoDbClient.Database(config["database"])
+			storages = append(storages, storage.NewMongoStorage(ctx, db.Collection(config["collection"])))
+		}
+	}
+
+	fetcherConfig := viper.GetStringMapString("fetchers.freecurrconv")
+	maxPerHour, err := strconv.ParseUint(fetcherConfig["maxPerHour"], 10, 32)
+
+	if err != nil {
+		log.Fatalf("Error while parsing maxPerHour in fetchers.freecurrconv: %v", err)
+	}
+
+	maxPerRequest, err := strconv.ParseUint(fetcherConfig["maxPerRequest"], 10, 32)
+	if err != nil {
+		log.Fatalf("Error while parsing maxPerRequest in fetchers.freecurrconv: %v", err)
+	}
+
+
+	service := services.FreeConvService{
+		Fetcher: currency.FreeCurrConvFetcher{
+			ApiKey:        fetcherConfig["apiKey"],
+			MaxPerHour:    int(maxPerHour),
+			MaxPerRequest: int(maxPerRequest),
+		},
+		Storage: storages,
+	}
+
+
+	_, err = service.Save(viper.GetStringSlice("currencies"))
+
+	if err != nil {
+		log.Fatalf("Error while saving currencies: %v", err)
+	}
+
+	if mongoDbClient != nil && mongoDbClient.Disconnect(ctx) == nil {
+		log.Println("Disconnecting from mongodb")
+	}
+
+	if sqlDb != nil && sqlDb.Close() == nil {
+		log.Println("Disconnecting from SQL Database")
+	}
 }
