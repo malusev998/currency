043f41b5ba26e789f13c9f0ead9c83dcea2f20ee
Dusan Malusev <dusan.998@outlook.com>
2020-10-11 00:57:23 +0200

    Adding CLI command integration test
    Signed-off-by: Dusan Malusev <dusan.998@outlook.com>

--- cli/cmd/fetch.go
+++ cli/cmd/fetch.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"log"
+	"os"
 	"time"
 
 	"github.com/spf13/cobra"
@@ -11,7 +12,10 @@ func fetch(config *Config) *cobra.Command {
 	var standalone bool
 	var after time.Duration
 
-	handle := func() error {
+	handle := func(logger *log.Logger) error {
+
+		log.SetOutput(os.Stdout)
+		defer log.SetOutput(os.Stderr)
 		currenciesMap, err := config.CurrencyService.Save(config.CurrenciesToFetch)
 
 		if err != nil {
@@ -24,7 +28,7 @@ func fetch(config *Config) *cobra.Command {
 
 		for storage, currencies := range currenciesMap {
 			for i, currency := range currencies {
-				log.Printf("%d\tCurrency %s_%s saved to %s: Rate: %f\n", i, currency.From, currency.To, storage, currency.Rate)
+				logger.Printf("%d\tCurrency %s_%s saved to %s: Rate: %f\n", i, currency.From, currency.To, storage, currency.Rate)
 			}
 		}
 
@@ -33,13 +37,19 @@ func fetch(config *Config) *cobra.Command {
 
 	fetchCmd := &cobra.Command{
 		Use: "fetch",
-		Run: func(cmd *cobra.Command, args []string) {
+	}
+
+	logger := log.New(fetchCmd.OutOrStdout(), "fetch ", 0)
+	errLogger := log.New(fetchCmd.OutOrStderr(), "fetch-error ", 0)
+
+	fetchCmd.Run = func(logger, errLogger *log.Logger) func(cmd *cobra.Command, args []string) {
+		return func(cmd *cobra.Command, args []string) {
 			if standalone {
 				for {
 					select {
 					case <-time.After(after):
-						if err := handle(); err != nil {
-							log.Printf("ERROR: %v", err)
+						if err := handle(logger); err != nil {
+							errLogger.Printf("ERROR: %v", err)
 						}
 					case <-config.Ctx.Done():
 						return
@@ -47,11 +57,11 @@ func fetch(config *Config) *cobra.Command {
 				}
 			}
 
-			if err := handle(); err != nil {
-				log.Printf("ERROR: %v", err)
+			if err := handle(logger); err != nil {
+				errLogger.Printf("ERROR: %v", err)
 			}
-		},
-	}
+		}
+	}(logger, errLogger)
 
 	fetchCmd.Flags().BoolVar(&standalone, "standalone", false, "Start up a long running fetching service")
 	fetchCmd.Flags().DurationVar(&after, "after", time.Duration(1)*time.Hour, "Fetching for standalone process")
---
+++ cli/cmd/fetch_test.go
@@ -0,0 +1,145 @@
+package cmd
+
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/stretchr/testify/require"
+
+	currency_fetcher "github.com/BrosSquad/currency-fetcher"
+	"github.com/BrosSquad/currency-fetcher/currency"
+	"github.com/BrosSquad/currency-fetcher/services"
+	"github.com/BrosSquad/currency-fetcher/storage"
+)
+
+const mysqlTableName = "currency_integration_test"
+
+type (
+	httpMock  struct{}
+	mysqlData struct {
+		ID        string
+		Currency  string
+		Provider  string
+		Rate      float32
+		CreatedAt string
+	}
+)
+
+func (h httpMock) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	payload, _ := json.Marshal(map[string]float32{
+		"EUR_USD": 1.2,
+		"USD_EUR": 0.8,
+	})
+
+	writer.WriteHeader(200)
+	writer.Write(payload)
+}
+
+func storages(t *testing.T, ctx context.Context) ([]currency_fetcher.Storage, *sql.DB) {
+	st := make([]currency_fetcher.Storage, 0)
+	runningInDocker := false
+
+	if os.Getenv("RUNNING_IN_DOCKER") != "" {
+		runningInDocker = true
+	}
+
+	mysqlDriverConfig := mysql.NewConfig()
+	mysqlDriverConfig.User = "currency"
+	mysqlDriverConfig.Passwd = "currency"
+	mysqlDriverConfig.DBName = "currencydb"
+	connectionString := mysqlDriverConfig.FormatDSN()
+
+	if runningInDocker {
+		mysqlDriverConfig.Addr = "mysql:3306"
+	} else {
+		mysqlDriverConfig.Addr = "localhost:3306"
+	}
+
+	db, err := sql.Open("mysql", connectionString)
+
+	if err != nil {
+		t.FailNow()
+	}
+
+	mysql := storage.NewMySQLStorage(ctx, db, mysqlTableName, nil)
+
+	if err := mysql.Migrate(); err != nil {
+		t.FailNow()
+	}
+
+	st = append(st, mysql)
+
+	return st, db
+}
+
+func testMySQLDataSet(asserts *require.Assertions, rows *sql.Rows) {
+	mysqlDataSet := make([]mysqlData, 0)
+
+	for rows.Next() {
+		set := mysqlData{}
+		rows.Scan(&set.ID, &set.Currency, &set.Provider, &set.Rate, &set.CreatedAt)
+		mysqlDataSet = append(mysqlDataSet, set)
+	}
+
+	asserts.Len(mysqlDataSet, 2)
+
+	for _, set := range mysqlDataSet {
+		asserts.Contains([]string{"EUR_USD", "USD_EUR"}, set.Currency)
+		asserts.Equal(currency.FreeConvProvider, set.Provider)
+		asserts.Contains([]float32{1.2, 0.8}, set.Rate)
+	}
+}
+
+func TestFetchCommand(t *testing.T) {
+	t.Parallel()
+	asserts := require.New(t)
+	debug := false
+	ctx := context.Background()
+	server := httptest.NewServer(httpMock{})
+
+	defer server.Close()
+
+	fetcher := currency.FreeCurrConvFetcher{
+		ApiKey:        "123456",
+		MaxPerHour:    100,
+		MaxPerRequest: 2,
+		Url:           server.URL,
+	}
+
+	st, mysqlDb := storages(t, ctx)
+
+	defer mysqlDb.ExecContext(ctx, fmt.Sprintf("DROP TABLE %s;", mysqlTableName))
+
+	currencyService := services.FreeConvService{
+		Fetcher: fetcher,
+		Storage: st,
+	}
+
+	config := Config{
+		Ctx:               ctx,
+		debug:             &debug,
+		CurrenciesToFetch: []string{"EUR_USD", "USD_EUR"},
+		CurrencyService:   currencyService,
+	}
+
+	t.Run("Without Debug", func(t *testing.T) {
+		cmd := fetch(&config)
+		asserts.Nil(cmd.Execute())
+
+		rows, err := mysqlDb.Query(fmt.Sprintf("SELECT * FROM %s;", mysqlTableName))
+
+		asserts.Nil(err)
+
+		defer rows.Close()
+
+		testMySQLDataSet(asserts, rows)
+	})
+
+}
